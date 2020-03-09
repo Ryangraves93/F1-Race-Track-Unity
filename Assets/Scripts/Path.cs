9976f5215474a14335fee331e297cd446d528454
Ryangraves93 <ryangraves1993@gmail.com>
2020-03-09 19:20:14 +0000

    Began Implementing Spline Editor

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path 
+{
+    [SerializeField, HideInInspector]
+    List<Vector2> points;
+
+    public Path(Vector2 centre)
+    {
+        points = new List<Vector2>
+        {
+            centre+Vector2.left,
+            centre+(Vector2.left+Vector2.up)*.5f,
+            centre + (Vector2.right+Vector2.down)*.5f,
+            centre + Vector2.right
+        };
+    }
+
+    public Vector2 this[int i]
+    {
+        get {
+            return points[i];
+            }
+    }
+    public int NumPoints
+    {
+        get
+        {
+            return points.Count;
+        }
+    }
+    public int NumSegments
+    {
+        get
+        {
+            return (points.Count - 4) / 3 + 1;
+        }
+    }
+
+    public void AddSegment(Vector2 anchorPos)
+    {
+        points.Add(points[points.Count - 1] * 2 - points[points.Count - 2]);
+        points.Add((points[points.Count - 1] + anchorPos) * .5f);
+        points.Add(anchorPos);
+
+    }
+
+    public Vector2[] GetPointsInSegment(int i)
+    {
+        return new Vector2[] { points[1 * 3], points[i * 3 + 1], points[i * 3 + 2], points[i * 3 + 3] };
+    }
+
+    public void MovePoint (int i, Vector2 pos)
+    {
+        points[i] = pos;
+    }
+}
---
+++ Assets/Scripts/PathEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+    PathCreator creator;
+    Path path;
+
+    private void OnSceneGUI()
+    {
+        Draw();
+    }
+
+    void Draw()
+    {
+
+        for (int i = 0; i < path.NumSegments; i++)
+        {
+            Vector2[] points = path.GetPointsInSegment(i);
+            Handles.color = Color.red;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+        }
+        for (int i = 0; i < path.NumPoints; i ++)
+        {
+            Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector2.zero, Handles.CylinderHandleCap);
+            if (path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move Point");
+                path.MovePoint(i, newPos);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        creator = (PathCreator)target;
+
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+        path = creator.path;
+    }
+}
