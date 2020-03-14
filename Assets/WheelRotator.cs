9cca2f25fca88efd9efb29d7bb4a106dc0b50512
Ryangraves93 <ryangraves1993@gmail.com>
2020-03-14 18:23:32 +0000

    Pre wheel collider

--- Assets/WheelRotator.cs
+++ Assets/WheelRotator.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class WheelRotator : MonoBehaviour
 {
 
-    
+    public Quaternion frontWheelRight, frontWheelLeft;
+    public Transform rearWheelRight, rearWheelLeft;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,11 @@ public class WheelRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rotateWheels();
+    }
+
+    void rotateWheels()
+    {
+        frontWheelLeft.x += .5f;
     }
 }
