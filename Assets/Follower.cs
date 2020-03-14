224c02358292d894bd06fc8dccdca7af8f687e60
Ryangraves93 <ryangraves1993@gmail.com>
2020-03-14 09:35:17 +0000

    Implemented path finding using spline

---
+++ Assets/Follower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTraveled;
+
+    private void Update()
+    {
+         {
+            distanceTraveled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTraveled);
+        }
+    }
+}
---
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 5;
+        float distanceTravelled;
+        public float timeToStart;
+        private bool readyToGo = false;
+
+        void Start()
+        {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+
+            StartCoroutine(carReady());
+
+        }
+
+        void Update()
+        {
+            if (readyToGo == true)
+            {
+
+                if (pathCreator != null)
+                {
+                    distanceTravelled += speed * Time.deltaTime;
+                    transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                    transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                }
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+
+        public IEnumerator carReady()
+        {
+            yield return new WaitForSeconds(timeToStart);
+            readyToGo = true;
+        }
+    }
+}
